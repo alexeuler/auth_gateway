1238725c94c7f02201e1aac10e7b86df40453c9f
Alexey Karasev <karasev.alexey@gmail.com>
2017-01-04 21:33:35 +0500

    some forms

--- app/controllers/Sessions.scala
+++ app/controllers/Sessions.scala
@@ -3,12 +3,40 @@ package controllers
 import com.google.inject.Inject
 import models.{User, UserRepo}
 import play.api._
+import play.api.data._
+import play.api.data.Forms._
 import play.api.mvc._
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 class Sessions @Inject()(userRepo: UserRepo)(implicit exec: ExecutionContext) extends Controller {
-  def make = Action {
-      Ok(views.html.sessions.make())
+
+  val userForm: Form[User] = Form(
+    mapping(
+      "email" -> nonEmptyText,
+      "password" -> nonEmptyText
+    )(
+      (email: String, password: String) => new User(email, password)
+    )(user => Some(user.email, ""))
+  )
+
+  def make = Action.async { implicit request =>
+    userForm.bindFromRequest().fold(
+      errors => {
+        Future { Ok(views.html.sessions.make()) }
+      },
+      userData => {
+        for {
+          user <- userRepo.queries.findByCredentials(userData.email, userData.password)
+        } yield user match {
+            case None => Ok(views.html.sessions.make())
+            case _ => Ok(views.html.sessions.make())
+        }
+      }
+    )
+  }
+
+  def create = Action {
+
   }
 }
