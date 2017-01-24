9f48971693b0bfc2c80b984df8bc9d0ac2704d84
Alexey Karasev <karasev.alexey@gmail.com>
2017-01-24 20:27:58 +0500

    moved sessions into auth package

--- app/controllers/auth/Sessions.scala
+++ app/controllers/auth/Sessions.scala
@@ -1,10 +1,10 @@
-package controllers
+package controllers.auth
 
 import com.google.inject.Inject
 import models.{User, UserRepo}
-import play.api.data._
 import play.api.data.Forms._
-import play.api.i18n.{I18nSupport, Lang, Messages, MessagesApi}
+import play.api.data._
+import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc._
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -22,16 +22,16 @@ class Sessions @Inject()(userRepo: UserRepo, val messagesApi: MessagesApi)(impli
     userForm.bindFromRequest().fold(
       formWithErrors => {
         Future {
-          Ok(views.html.sessions.make(formWithErrors))
+          Ok(views.html.auth.sessions.make(formWithErrors))
         }
       },
       userData => {
-        Future { Ok(views.html.sessions.make(userForm)) }
+        Future { Ok(views.html.auth.sessions.make(userForm)) }
       }
     )
   }
 
   def make = Action { implicit request =>
-    Ok(views.html.sessions.make(userForm.discardingErrors))
+    Ok(views.html.auth.sessions.make(userForm.discardingErrors))
   }
 }
