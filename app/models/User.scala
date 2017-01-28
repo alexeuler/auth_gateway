cced831b9086d5eef2ef4b012b504fb9ce723669
Alexey Karasev <karasev.alexey@gmail.com>
2017-01-29 02:22:02 +0500

    refactoring

--- app/models/User.scala
+++ app/models/User.scala
@@ -11,20 +11,45 @@ import services.Encryption
 
 import scala.concurrent.Future
 
+object Provider extends Enumeration {
+  type Provider = Value
+  val Email = Value("email")
+}
+
+object Role extends Enumeration {
+  type Role = Value
+  val Unconfirmed = Value("unconfirmed")
+  val User = Value("user")
+  val Admin = Value("admin")
+}
+
+import Provider._
+import Role._
+
 case class User(id: Long = 0L,
                 createdAt: Timestamp = new Timestamp(0L),
                 updatedAt: Timestamp = new Timestamp(0L),
-                provider: String,
+                provider: Provider,
                 email: String,
-                password: String) extends Identity
+                password: String,
+                role: Role
+               ) extends Identity
 
 object User {
-  def apply(provider: String, email: String, password: String): User =
-    User(0L, new Timestamp(0L), new Timestamp(0L), provider, email.toLowerCase, Encryption.md5(password))
+  def apply(provider: Provider, email: String, password: String): User =
+    User(
+      0L,
+      new Timestamp(0L),
+      new Timestamp(0L),
+      provider,
+      email.toLowerCase,
+      Encryption.md5(password),
+      if (provider == Email) Role.Unconfirmed else Role.User
+    )
 
-  def apply(email: String, password: String): User = apply("email", email, password)
+  def apply(email: String, password: String): User = apply(Email, email, password)
 
-  def tupled = (User.apply: (Long, Timestamp, Timestamp, String, String, String) => User).tupled
+  def tupled = (User.apply: (Long, Timestamp, Timestamp, Provider, String, String, Role) => User).tupled
 }
 
 trait UserRepo {
@@ -37,16 +62,27 @@ class UserRepoImpl @Inject()(override val dbConfigProvider: DatabaseConfigProvid
   extends BaseRepo[User](dbConfigProvider) with UserRepo {
   import driver.api._
 
+  implicit val roleMapper = MappedColumnType.base[Role, String](
+    action => action.toString,
+    string => Role.withName(string)
+  )
+
+  implicit val providerMapper = MappedColumnType.base[Provider, String](
+    action => action.toString,
+    string => Provider.withName(string)
+  )
+
   class EntityTable(tag: Tag) extends BaseTable(tag, "users") {
-    def provider = column[String]("provider")
+    def provider = column[Provider]("provider")
     def email = column[String]("email")
     def password = column[String]("password")
-    override def * = (id, createdAt, updatedAt, provider, email, password) <> (User.tupled, User.unapply)
+    def role = column[Role]("role")
+    override def * = (id, createdAt, updatedAt, provider, email, password, role) <> (User.tupled, User.unapply)
   }
 
   object UserQueries {
     def filter(loginInfo: LoginInfo): Query[EntityTable, User, Seq] =
-      for (user <- query; if (user.email === loginInfo.providerKey) && (user.provider === loginInfo.providerID)) yield user
+      for (user <- query; if (user.email === loginInfo.providerKey) && (user.provider === Provider.withName(loginInfo.providerID))) yield user
   }
 
   object UserActions {
--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -17,6 +17,7 @@ CREATE TABLE users (
   provider VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
+  role VARCHAR(255) NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
   updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp
 );
