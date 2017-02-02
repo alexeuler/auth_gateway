8ed2e0c56252f61e7fecb08a99609422e9c45db4
Alexey Karasev <karasev.alexey@gmail.com>
2017-02-03 01:22:38 +0500

    split code of repo and model

---
+++ app/models/TokenRepo.scala
@@ -0,0 +1,50 @@
+package models
+
+import java.sql.Timestamp
+
+import com.google.inject.{Inject, Singleton}
+import models.TokenAction.TokenAction
+import play.api.db.slick.DatabaseConfigProvider
+
+import scala.concurrent.Future
+
+trait TokenRepo {
+  def find(value: String): Future[Option[Token]]
+  def create(token: Token): Future[Token]
+  def delete(id: Long): Future[Int]
+}
+
+@Singleton
+class TokenRepoImpl @Inject()(override val dbConfigProvider: DatabaseConfigProvider)
+  extends BaseRepo[Token](dbConfigProvider) with TokenRepo {
+
+  import driver.api._
+
+  implicit val tokenActionMapper = MappedColumnType.base[TokenAction, String](
+    action => action.toString,
+    string => TokenAction.withName(string)
+  )
+
+  class EntityTable(tag: Tag) extends BaseTable(tag, "tokens") {
+    def payload = column[String]("payload")
+    def value = column[String]("value")
+    def action = column[TokenAction]("action")
+    def expirationTime = column[Timestamp]("expiration_time")
+
+    override def * = (id, createdAt, updatedAt, payload, action, value, expirationTime) <> (Token.tupled, Token.unapply)
+  }
+
+  override def query = TableQuery[EntityTable]
+
+  object Queries {
+    def filter(value: String): Query[EntityTable, Token, Seq] =
+      for (token <- query; if token.value === value) yield token
+  }
+
+  object Actions {
+    def find(value: String): DBIO[Option[Token]] = Queries.filter(value).result.headOption
+  }
+
+  override def find(value: String): Future[Option[Token]] = db.run(Actions.find(value))
+}
+
---
+++ app/models/UserRepo.scala
@@ -0,0 +1,55 @@
+package models
+
+import com.google.inject.{Inject, Singleton}
+import com.mohiva.play.silhouette.api.LoginInfo
+import models.Provider.Provider
+import models.Role.Role
+import play.api.db.slick.DatabaseConfigProvider
+
+import scala.concurrent.Future
+
+trait UserRepo {
+  def find(loginInfo: LoginInfo): Future[Option[User]]
+  def create(user: User): Future[User]
+  def updateRole(loginInfo: LoginInfo, role: Role): Future[Int]
+}
+
+@Singleton
+class UserRepoImpl @Inject()(override val dbConfigProvider: DatabaseConfigProvider)
+  extends BaseRepo[User](dbConfigProvider) with UserRepo {
+  import driver.api._
+
+  implicit val roleMapper = MappedColumnType.base[Role, String](
+    action => action.toString,
+    string => Role.withName(string)
+  )
+
+  implicit val providerMapper = MappedColumnType.base[Provider, String](
+    action => action.toString,
+    string => Provider.withName(string)
+  )
+
+  class EntityTable(tag: Tag) extends BaseTable(tag, "users") {
+    def provider = column[Provider]("provider")
+    def email = column[String]("email")
+    def password = column[String]("password")
+    def role = column[Role]("role")
+    override def * = (id, createdAt, updatedAt, provider, email, password, role) <> (User.tupled, User.unapply)
+  }
+
+  object UserQueries {
+    def filter(loginInfo: LoginInfo): Query[EntityTable, User, Seq] =
+      for (user <- query; if (user.email === loginInfo.providerKey) && (user.provider === Provider.withName(loginInfo.providerID))) yield user
+  }
+
+  object UserActions {
+    def find(loginInfo: LoginInfo): DBIO[Option[User]] = UserQueries.filter(loginInfo).result.headOption
+    def updateRole(loginInfo: LoginInfo, role: Role): DBIO[Int] =
+      UserQueries.filter(loginInfo).map(_.role).update(role)
+  }
+
+  override def query = TableQuery[EntityTable]
+
+  override def find(loginInfo: LoginInfo): Future[Option[User]] = db.run(UserActions.find(loginInfo))
+  def updateRole(loginInfo: LoginInfo, role: Role): Future[Int] = db.run(UserActions.updateRole(loginInfo, role))
+}
